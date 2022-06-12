02518abc0336c199411894d10e72e28709782904
cavalenciad <cavalenciad@outlook.com>
2022-06-12 13:39:30 -0500

    actualización de puerto

--- app.js
+++ app.js
@@ -22,4 +22,4 @@ app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html"));
    });
 
-app.listen(port, () => console.log(`Listening on ${port}`))
+app.listen(port, () => console.log(`Listening on ${port}`));
